2b24406ba0e0f256d07a7d088f1162e21a506b9f
prashanth-g <prashanthcsgce@gmail.com>
2019-06-04 17:46:12 +0530

    Added boot class

---
+++ src/main/java/com/prashanth/spring/functional/dsl/SpringFunctionalDSL.java
@@ -0,0 +1,11 @@
+package com.prashanth.spring.functional.dsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFunctionalDSL {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFunctionalDSL.class, args);
+    }
+}
