a8892679796d93137e2c1e71fc720e833f981606
prashanth-g <prashanthcsgce@gmail.com>
2019-06-15 22:21:50 +0530

    Added POST route

--- src/main/java/com/prashanth/spring/functional/dsl/PersonService.java
+++ src/main/java/com/prashanth/spring/functional/dsl/PersonService.java
@@ -3,17 +3,27 @@ package com.prashanth.spring.functional.dsl;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
 
+  private final AtomicLong counter = new AtomicLong();
+
   private final Set<Person> people = new HashSet<>(
-      Arrays.asList(new Person(1L, "Sachin"), new Person(2L, "Sehwag"),
-          new Person(3L, "Ganguly")));
+      Arrays.asList(new Person(counter.incrementAndGet(), "Sachin"),
+          new Person(counter.incrementAndGet(), "Sehwag"),
+          new Person(counter.incrementAndGet(), "Ganguly")));
 
   Set<Person> all() {
     return this.people;
+  }
+
+  Person save(Person p) {
+    Person person = new Person(counter.incrementAndGet(), p.getName());
+    this.people.add(person);
+    return person;
   }
 
   Person byId(Long id) {
--- src/main/java/com/prashanth/spring/functional/dsl/SpringFunctionalDSL.java
+++ src/main/java/com/prashanth/spring/functional/dsl/SpringFunctionalDSL.java
@@ -1,11 +1,13 @@
 package com.prashanth.spring.functional.dsl;
 
+import java.net.URI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class SpringFunctionalDSL {
@@ -21,6 +23,13 @@ public class SpringFunctionalDSL {
             request -> ServerResponse.ok().syncBody(personService.all()))
         .GET("/people/{id}", request -> ServerResponse.ok().syncBody(
             personService.byId(Long.parseLong(request.pathVariable("id")))))
+        .POST("/people", request -> {
+          Mono<Person> person = request.bodyToMono(Person.class);
+          String name = person.block().getName();
+          Person savedPerson = personService.save(new Person(null, name));
+          URI uri = URI.create("/people/" + savedPerson.getId());
+          return ServerResponse.created(uri).syncBody(savedPerson);
+        })
         .build();
   }
 
